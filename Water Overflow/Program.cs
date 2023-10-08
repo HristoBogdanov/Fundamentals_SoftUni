71ee4590d2f280a499bbc40361511a278a2e9341
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-10-08 18:21:16 +0300

    Add files via upload

---
+++ Water Overflow/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int capacity = 0;
+            int inLiters= 0;
+            for (int i = 0; i < n; i++)
+            {
+                inLiters = int.Parse(Console.ReadLine());
+                if (inLiters>255-capacity) Console.WriteLine("Insufficient capacity!");
+                else capacity += inLiters;
+            }
+            Console.WriteLine(capacity);
+        }
+    }
+}
